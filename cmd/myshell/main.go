20ac4d72de001f477e4e3f24d0ee696d473be337
Christopher Leggett <chris@leggett.dev>
2024-05-29 12:09:54 -0400

    Refactor REPL slightly

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -15,7 +15,11 @@ func main() {
 	    // Print the prompt
         fmt.Fprint(os.Stdout, "$ ")
 	    // Wait for user input
-        text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+        text, err := bufio.NewReader(os.Stdin).ReadString('\n')
+        if err != nil {
+            fmt.Fprintf(os.Stderr, "error while processing stdin: %s", err)
+            os.Exit(1)
+        }
         fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimSpace(text))
     }
 }
