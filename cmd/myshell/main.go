0cb549b69e40736087e5a2576b16bec61bba0229
Christopher Leggett <chris@leggett.dev>
2024-05-29 12:03:56 -0400

    Add REPL

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -11,10 +11,11 @@ func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	// fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
-
-	// Wait for user input
-    text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
-    fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimSpace(text))
+    for {
+	    // Print the prompt
+        fmt.Fprint(os.Stdout, "$ ")
+	    // Wait for user input
+        text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+        fmt.Fprintf(os.Stdout, "%s: command not found\n", strings.TrimSpace(text))
+    }
 }
